7a0bf5498f1c1f524ca6a528303685a78e4ba7db
Wednesday Rabbitt <28800494+WednesdayRabbitt@users.noreply.github.com>
2018-11-21 20:14:55 +1300

    Update Ring_light.ino

--- Ring_light/Ring_light.ino
+++ Ring_light/Ring_light.ino
@@ -9,14 +9,17 @@
 #define NUMPIXELS       24      //number of pixels on ring
 #define RING_PIN        14      //pin for ring
 #define BUTTON_PIN      4       //pin for button
-#define delayval        30      // delay for half a second
+#define DELAY           50      //delay between pixels lighting up
+#define SKIP            5       //number of pixels to skip when lighing up (1,5,7,11,13,17,19,23)
+
+int buttonState = HIGH;
+int t = 0;                      //time variable
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 // Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
 // example for more information on possible values.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, RING_PIN, NEO_GRBW + NEO_KHZ800);
 
-int buttonState = HIGH;
 
 void setup() {
   pixels.begin(); // This initializes the NeoPixel library.
@@ -30,12 +33,14 @@ void loop() {
   // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
   
   for(int i=0;i<NUMPIXELS;i++){
-    int j = (i*5)%24;
+    int j = (i)*SKIP%24;
+    t = millis();
+    
     if (buttonState == HIGH){
       pixels.setPixelColor(j, pixels.Color(0,0,50)); // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
       
       pixels.show(); // This sends the updated pixel color to the hardware.
-      delay(delayval); // Delay for a period of time (in milliseconds).
+      delay(DELAY); // Delay for a period of time (in milliseconds).
     } else {
       pixels.setPixelColor(i, pixels.Color(0,0,0));
       pixels.show();
