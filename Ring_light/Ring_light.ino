c942e6c76a9947f763dad35e64189869f2978675
Wednesday Rabbitt <28800494+WednesdayRabbitt@users.noreply.github.com>
2018-11-21 18:59:55 +1300

    Update Ring_light.ino

--- Ring_light/Ring_light.ino
+++ Ring_light/Ring_light.ino
@@ -6,29 +6,39 @@
   #include <avr/power.h>
 #endif
 
-#define PIN            14
-#define NUMPIXELS      24
+#define NUMPIXELS       24      //number of pixels on ring
+#define RING_PIN        14      //pin for ring
+#define BUTTON_PIN      4       //pin for button
+#define delayval        30      // delay for half a second
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 // Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
 // example for more information on possible values.
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRBW + NEO_KHZ800);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, RING_PIN, NEO_GRBW + NEO_KHZ800);
 
-int delayval = 50; // delay for half a second
+int buttonState = HIGH;
 
 void setup() {
   pixels.begin(); // This initializes the NeoPixel library.
+  pinMode(BUTTON_PIN, INPUT);
 }
 
 void loop() {
 
+  buttonState = digitalRead(BUTTON_PIN);
+  
   // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
-
+  
   for(int i=0;i<NUMPIXELS;i++){
-
-    pixels.setPixelColor((i*11)%24, pixels.Color(0,50,20)); // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-
-    pixels.show(); // This sends the updated pixel color to the hardware.
-    delay(delayval); // Delay for a period of time (in milliseconds).
+    int j = (i*5)%24;
+    if (buttonState == HIGH){
+      pixels.setPixelColor(j, pixels.Color(0,0,50)); // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+      
+      pixels.show(); // This sends the updated pixel color to the hardware.
+      delay(delayval); // Delay for a period of time (in milliseconds).
+    } else {
+      pixels.setPixelColor(i, pixels.Color(0,0,0));
+      pixels.show();
+    }
   }
 }
